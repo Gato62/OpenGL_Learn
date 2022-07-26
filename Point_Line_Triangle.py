513756d19ff2fedbb73c64b657d7bc392e56bb4d
Gato62 <gato62@vk.com>
2022-07-27 01:00:33 +0300

    added triangle

--- Point_Line_Triangle.py
+++ Point_Line_Triangle.py
@@ -8,47 +8,38 @@ def point():
     glPointSize(7)              # Point size
 
     glBegin(GL_POINTS)          # Draw points
-    glColor3f(.9, .9, .9)       # Dolor points
-
-    glVertex3f(0.0, 0.0, 0.0)   # point 1
-    glVertex3f(0.0, 1.0, 0.0)   # point 2
-    glVertex3f(1.0, 1.0, 0.0)   # point 3
-    glVertex3f(1.0, 0.0, 0.0)   # point 4
+    glColor3f(.9, .9, .9)       # Color points
+    glVertex2f(0.0, 0.0)  # point 1
+    glVertex2f(1.0, 0.0)  # point 2
+    glVertex2f(0.0, 1.0)  # point 3
     glEnd()
 
 
 def edge():
-    glLineWidth(2)              # Point size
+    glLineWidth(2)              # Line size
 
-    glBegin(GL_LINE_LOOP)       # Draw points
-    glColor3f(.2, .2, .2)       # Dolor points
-
-    glVertex3f(0.0, 0.0, 0.0)   # point 1
-    glVertex3f(0.0, 1.0, 0.0)   # point 2
-    glVertex3f(1.0, 1.0, 0.0)   # point 3
-    glVertex3f(1.0, 0.0, 0.0)   # point 4
+    glBegin(GL_LINE_LOOP)       # Draw lines
+    glColor3f(.2, .2, .2)       # Color points
+    glVertex2f(0.0, 0.0)  # point 1
+    glVertex2f(1.0, 0.0)  # point 2
+    glVertex2f(0.0, 1.0)  # point 3
     glEnd()
 
 
 def triangle():
-    glBegin(GL_TRIANGLES)       # Draw triangle
-    glColor3f(.2, .6, .2)       # Dolor points
-
-    glVertex3f(0.0, 0.0, 0.0)   # point 1
-    glColor3f(.1, .2, .7)
-    glVertex3f(0.0, 1.0, 0.0)   # point 2
-    glColor3f(.7, .3, .1)
-    glVertex3f(1.0, 1.0, 0.0)   # point 3
+    glBegin(GL_TRIANGLES)      # Draw triangle
+    glColor3f(.0, .0, .0), glVertex2f(0.0, 0.0)  # point 1
+    glColor3f(.1, .7, .2), glVertex2f(1.0, 0.0)  # point 2
+    glColor3f(.7, .3, .2), glVertex2f(0.0, 1.0)  # point 3
     glEnd()
 
 
 def main(width, height):
     pygame.init()
-    display = (width, height)                                   # Display resolution
+    display = (width, height)                                  # Display resolution
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)    # Perspective
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)   # Perspective
 
-    glTranslatef(-.4, -0.5, -2.0)
 
     while True:
         for event in pygame.event.get():                        # Checking for quit
@@ -57,17 +48,26 @@ def main(width, height):
                 quit()
 
         glClearColor(.4, .4, .4, .0)                            # Color of the screen
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)      # Screen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)      # Screen cleaning
 
-# Draw objects
+        glLoadIdentity()
+        glScalef(.3, .3, 1)
+        glTranslatef(.4, .4, .0)
 
-        edge()
-        triangle()
-        point()
+        for index in range(220):
+            # glLoadIdentity()                                        # Coordinate reset
+            glPushMatrix()
+            glScalef(.8, .8, 1)
+            glRotatef(23 * index - index, 0, 0, 1)
+            glTranslatef(.1, .5, .0)
+            triangle()
+            glPopMatrix()
+
+
 
         pygame.display.flip()
-        pygame.time.wait(10)
+        pygame.time.wait(20)
 
 
 if __name__ == '__main__':
-    main(800, 600)
+    main(1000, 1000)
